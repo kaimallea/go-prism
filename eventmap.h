4a45d6ff373a0fc58aba87ddd31b184ab4709961
Kai Mallea <kmallea@gmail.com>
2013-07-15 01:03:13 -0400

    Add eventmap.h

---
+++ eventmap.h
@@ -0,0 +1,21 @@
+/**
+ * vim: set ts=4 sw=4 tw=99 noet :
+ * ======================================================
+ * Metamod:Source GoPrism Plugin
+ * Written by Kai Mallea
+ * ======================================================
+ *
+ */
+
+#ifndef _INCLUDE_EVENTMAP_H_
+#define _INCLUDE_EVENTMAP_H_
+
+#include <map>
+#include <string>
+
+class GoPrismPlugin;
+
+typedef void (GoPrismPlugin::*callback_func_t)(IGameEvent *event);
+typedef std::map<std::string, callback_func_t> event_map_t;
+
+#endif // _INCLUDE_EVENTMAP_H_
